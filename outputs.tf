f319bac190327b8b1f9601129385c65ae0e7ee0b
Craig Thacker <craigthackerx@gmail.com>
2025-05-25 00:42:36 +0100

    Update code

--- outputs.tf
+++ outputs.tf
@@ -18,27 +18,12 @@ output "service_endpoint_service_principal_id" {
   value       = azuredevops_serviceendpoint_azurerm.azure_devops_service_endpoint_azurerm.service_principal_id
 }
 
-output "user_assigned_managed_identity_id" {
-  description = "The resource id of the managed identity"
-  value       = azurerm_user_assigned_identity.uid.id
+output "managed_identity_client_id" {
+  description = "The client id of the managed identity"
+  value       = var.managed_identity_type != "SystemAssigned" ? azurerm_user_assigned_identity.uid.client_id : var.system_assigned_managed_identity_client_id
 }
 
-output "user_assigned_managed_identity_object_id" {
+output "managed_identity_object_id" {
   description = "The object id id of the managed identity"
-  value       = azurerm_user_assigned_identity.uid.principal_id
-}
-
-output "user_assigned_managed_identity_tenant_id" {
-  description = "The tenant id of the managed identity"
-  value       = azurerm_user_assigned_identity.uid.tenant_id
-}
-
-output "workload_identity_federation_issuer" {
-  description = "The issuer for the workload issuer"
-  value       = azuredevops_serviceendpoint_azurerm.azure_devops_service_endpoint_azurerm.workload_identity_federation_issuer
-}
-
-output "workload_identity_federation_subject" {
-  description = "The subject for the workload federation"
-  value       = azuredevops_serviceendpoint_azurerm.azure_devops_service_endpoint_azurerm.workload_identity_federation_subject
+  value       = var.managed_identity_type != "SystemAssigned" ? azurerm_user_assigned_identity.uid.principal_id: var.system_assigned_managed_identity_object_id
 }
